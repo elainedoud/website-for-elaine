56283530db5d3cf31ccde4fbb030cbd3aca94293
Elaine Doud <elainedoud@gmail.com>
2022-09-28 17:41:33 -0400

    Attempted to add Taylor Ham JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Let's celebrate the Garden State!";
 document.querySelector("body").appendChild(h2);
+
+const taylor = document.getElementById("button");
+
+document.addEventListener("keydown", function(event) {
+    if(event.key === "click") {
+        taylor.style.color = black;
+    }
+});
